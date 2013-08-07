4d16e49f10dc6c0f011583b96efcbe17a475c4b4
web@functor-static-four <web@functor-static-four.go.jp>
2013-08-07 13:48:59 +0000

    recruit and response are both salted.

--- website/Contents/Salt.hs
+++ website/Contents/Salt.hs
@@ -7,11 +7,11 @@ import Prelude
 import Data.ByteString.Lazy.Char8 (pack, ByteString)
 import Data.Monoid((<>))
 import Data.String (IsString(..))
-import Data.Digest.Pure.SHA (sha256, showDigest)
+import Data.Digest.Pure.SHA (sha1, showDigest)
 
 
 salt :: (Show a, IsString a) => a -> a
-salt str = fromString $ showDigest $ sha256 $ saltString <> (pack $ show str)
+salt str = fromString $ showDigest $ sha1 $ saltString <> (pack $ show str)
 
 
 -- | It is assumed that non-team member cannot read the content of our repository.
--- website/Handler/Report.hs
+++ website/Handler/Report.hs
@@ -2,20 +2,23 @@ module Handler.Report where
 
 import Import
 
+import           Contents.Salt (salt)
 import           Control.Lens ((&),(%~))
+import           Control.Monad
 import qualified Data.Text as Text
 import           Safe (readMay)
-
 
 
 getReportR :: String -> Handler Html
 getReportR str = do
   liftIO $ print str
-  let reportMay :: Maybe (Rational, Text.Text)
+  let reportMay :: Maybe ((Rational, Text.Text), String)
       reportMay = readMay str
   case reportMay of
-    Nothing -> defaultLayout [whamlet| Presentation Error. |]
-    Just (newScore, commitId) -> do
+    Nothing -> notFound
+    Just (solution@(newScore, commitId) , saltStr)-> do
+      when (salt (show solution) /= saltStr) notFound
+      
       runDB $ do
         keyMay <- getBy (UniqueSubmission commitId)
         case (keyMay :: Maybe (Entity Submission) ) of
