4dfce9b1e2efbb3d25d45160fbd34b6d7c325136
Takayuki Muranushi <muranushi@gmail.com>
2013-08-08 20:54:21 +0900

    test validation of a program.

--- synthesis-test/exe-src/test-01.hs
+++ synthesis-test/exe-src/test-01.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Monad
 import Data.SBV
 
-main :: IO ()
+type MyInt = SWord16
+
+type Program = SWord16
+
+
+execute :: Program -> (MyInt, MyInt) -> Symbolic ()
+execute prog (inp, oup) = do
+  constrain $ oup .== inp + prog
+
 main = do
-  return ()
+  let examples :: [(MyInt, MyInt)]
+      examples = [(1,3), (15, 18)]
   
+  res <- sat $ do
+    prog <- sWord16 "x"
+    mapM_ (execute prog) examples
+    return $ prog .== prog 
+    
+  print res
