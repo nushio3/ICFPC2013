89e9f55904282d39130bafe2e37ab8af986ccf0c
Takayuki Muranushi <muranushi@gmail.com>
2013-08-09 21:54:56 +0900

    test separated.

--- solver/test/SBVSpec.hs
+++ solver/test/SBVSpec.hs
@@ -14,28 +14,13 @@ import Safe
 
 import BV
 import SBV
+import SBVTools (unSymbolM)
 import Convert
 import TestInputs
 
 import Debug.Trace(trace)
 import Text.Printf
 
-unsafeSExec ::  Program -> BitVector -> Maybe BitVector
-unsafeSExec prog i = unsafePerformIO $ do
-  let rvSymbol = "returnValue"
-  resp <- sat $ do
-    rv0 <- sExec prog $ fromIntegral i
-    rv1 <- symbolic rvSymbol
-    return $ rv0 .== rv1
-  let sresp :: String
-      sresp = show resp
-  let rcand :: [BitVector]
-      rcand = do -- list monad
-        let True = ("Satisfiable." `isPrefixOf` sresp)
-        rvLine <- filter (isInfixOf rvSymbol) $ lines sresp
-        let (_:_: val : _) = words $ rvLine
-        maybeToList $ readMay val
-  return $ headMay rcand
   
 spec :: Spec
 spec = do
@@ -45,5 +30,5 @@ spec = do
         let prog = readProgram src in
 --         trace (printf "prog %d = %d" x (exec prog x)) $
 --         trace (printf "prog %d = %s" x (show $ unsafeSExec prog x)) $
-        Just (exec prog x) == unsafeSExec prog x
+        Just (exec prog x) == unSymbolM (sExec prog) x
 
