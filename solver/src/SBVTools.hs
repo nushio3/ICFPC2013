47746a29b7963bf1007c3543831c148fac5f12ce
Takayuki Muranushi <muranushi@gmail.com>
2013-08-09 21:53:23 +0900

    sbv tools separate.

---
+++ solver/src/SBVTools.hs
@@ -0,0 +1,30 @@
+module SBVTools where
+
+import Data.List (isPrefixOf, isInfixOf)
+import Data.Maybe
+import Data.SBV
+import BV(BitVector)
+import SBV(SBitVector)
+import Safe (readMay, headMay)
+import System.IO.Unsafe
+
+unSymbolM :: (SBitVector -> Symbolic SBitVector)
+         -> BitVector -> Maybe BitVector
+             
+             
+unSymbolM prog i = unsafePerformIO $ do
+  let rvSymbol = "returnValue"
+  resp <- sat $ do
+    rv0 <- prog $ fromIntegral i
+    rv1 <- symbolic rvSymbol
+    return $ rv0 .== rv1
+  let sresp :: String
+      sresp = show resp
+  let rcand :: [BitVector]
+      rcand = do -- list monad
+        let True = ("Satisfiable." `isPrefixOf` sresp)
+        rvLine <- filter (isInfixOf rvSymbol) $ lines sresp
+        let (_:_: val : _) = words $ rvLine
+        maybeToList $ readMay val
+  return $ headMay rcand
+             
