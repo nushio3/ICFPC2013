dcbdfa466be925d2a0caedea43f656f9ed142259
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2013-08-09 22:04:06 +0900

    no message

--- solver/exe-src/rich-main.hs
+++ solver/exe-src/rich-main.hs
@@ -9,23 +9,33 @@ import Data.List
 import Data.Function
 import Control.Monad
 import Control.Applicative
+import Data.Reflection
 
 import SRichBV (equiv)
+import API
 
---main = do
---  con <- L.getContents
---  let Just objs = decode con :: Maybe [Value]
---  let sorted = sortBy (compare `on` (^?! key "size"._Integer)) objs
---  mapM_ solve sorted
+{-}
+main = do
+  con <- L.getContents
+  let Just objs = decode con :: Maybe [Value]
+  let sorted = sortBy (compare `on` (^?! key "size"._Integer)) objs
+  mapM_ solve sorted
+-}
 
 main = do
-  ps <- programs
-  forM_ [ (i, j, x, y) | (i, x) <- zip [1..] ps, (j, y) <- zip [1..] ps, i < j ] $ \(i, j, x, y) -> do
-    b <- x `equiv` y
-    when b $ do
-      print (i, x)
-      print (j, y)
-      putStrLn "====="
+  give (Token "0017eB6c6r7IJcmlTb3v4kJdHXt1re22QaYgz0KjvpsH1H") $ do
+    TrainingProblem _ tid tsize tpos <- train $ TrainRequest 5 ["if0", "plus", "and", "shr1"]
+    ans <- solve tsize tpos
+    print ans
+
+--main = do
+--  ps <- programs
+--  forM_ [ (i, j, x, y) | (i, x) <- zip [1..] ps, (j, y) <- zip [1..] ps, i < j ] $ \(i, j, x, y) -> do
+--    b <- x `equiv` y
+--    when b $ do
+--      print (i, x)
+--      print (j, y)
+--      putStrLn "====="
 
 programs :: IO [Program]
 programs = map read . lines <$> getContents
