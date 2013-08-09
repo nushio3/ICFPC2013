7ee292fc442e0621c83bca15da70daa3bbfc7209
Takayuki Muranushi <muranushi@gmail.com>
2013-08-09 16:37:52 +0900

    write converter.

--- solver/src/Convert.hs
+++ solver/src/Convert.hs
@@ -1,12 +1,32 @@
 module Convert where
 
-import qualified BV
+import qualified BV as B
+import qualified RichBV as R
+
+import qualified Data.Map as Map
 
 import Data.Monoid (mempty)
 import Text.Trifecta (parseString, Result(..))
 
-readProgram :: String -> BV.Program
+readProgram :: String -> B.Program
 readProgram str = 
-  case parseString BV.parseProgram mempty str of
+  case parseString B.parseProgram mempty str of
     Success x -> x
     Failure doc -> error $ show doc
+
+enrichProgram :: B.Program -> R.Program
+enrichProgram (B.Program x e) = 
+  R.Program $ convE (Map.singleton x 1) e
+  
+convE :: Map.Map B.Idfr Int -> B.Expr -> R.Expression   
+convE m B.C0 = R.Constant 0
+convE m B.C1 = R.Constant 1
+convE m (B.Var idfr) = R.Var $ m Map.! idfr
+  
+  {-
+data Expr = C0 | C1 | Var Idfr 
+    | If0 Expr Expr Expr
+    | Fold Expr Expr Reducer
+    | Op1 Op1 Expr
+    | Op2 Op2 Expr Expr deriving (Show, Eq, Ord)
+  -}
