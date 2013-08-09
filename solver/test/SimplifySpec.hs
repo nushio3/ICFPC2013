d30a626210bd57b0606e42d145a0130ac4f01980
Takayuki Muranushi <muranushi@gmail.com>
2013-08-09 19:05:20 +0900

    debugging

--- solver/test/SimplifySpec.hs
+++ solver/test/SimplifySpec.hs
@@ -10,6 +10,9 @@ import System.Environment (lookupEnv)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
+import Debug.Trace (trace)
+import Text.Printf
+
 import Safe
 
 import qualified BV as B
@@ -36,6 +39,7 @@ spec = do
     forM_ programs $ \src -> do
       prop ("prog == simplify prog for " ++ src) $ \x -> 
         let 
+          prog0 = readProgram src 
           prog = unsafePerformIO $ do
             let ret=enrichProgram $ readProgram src 
             print ret
@@ -45,5 +49,8 @@ spec = do
             print ret
             return ret
         in
+        trace (printf "prog0 %d = %d" x (B.exec prog0 x)) $          
+        trace (printf "prog %d = %d" x (R.eval prog x)) $ 
+        trace (printf "prog' %d = %d" x (R.eval prog' x)) $ 
         R.eval prog x == R.eval prog' x 
 
