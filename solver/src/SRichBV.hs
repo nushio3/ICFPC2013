20e1f88f325842149c5a74f1efa9c891caaf5025
Takayuki Muranushi <muranushi@gmail.com>
2013-08-09 16:18:01 +0900

    update util.

--- solver/src/SRichBV.hs
+++ solver/src/SRichBV.hs
@@ -4,8 +4,28 @@ import SBV (SBitVector)
 import RichBV
 import Data.SBV
 
-sExec :: Program -> (SBitVector -> Symbolic SBitVector)
+sExec :: Program -> SBitVector -> SBitVector
 sExec (Program e) x = sEvalE e [x]
 
-sEvalE :: Expression -> [SBitVector] -> Symbolic SBitVector
-sEvalE (Constant c) _ = return $ fromIntegral c
+sEvalE :: Expression -> [SBitVector] -> SBitVector
+sEvalE (Constant c) _ = fromIntegral c
+sEvalE (Var ix) env = env !! (ix - 1)
+sEvalE (If e1 e2 e3) env =
+  ite (sEvalE e1 env .== 0)
+      (sEvalE e2 env)
+      (sEvalE e3 env)
+sEvalE (Fold e1 e2 e3) env =
+  let v = map (.&. 0xFF) $ take 8 $ iterate (`shiftR` 8) $ sEvalE e1 env
+  in foldr (\w a -> sEvalE e3 $ env ++ [w, a]) (sEvalE e2 env) v
+  
+sEvalE (Op1 op e) env =
+  sEvalOp1 op $ sEvalE e env
+
+sEvalOp1 Not = complement
+sEvalOp1 (Shl n) = (`shiftL` n)
+sEvalOp1 (Shr n) = (`shiftR` n)
+
+sEvalOp2 And  = (.&.)
+sEvalOp2 Or   = (.|.)
+sEvalOp2 Xor  = xor
+sEvalOp2 Plus = (+)
--- solver/src/Util.hs
+++ solver/src/Util.hs
@@ -1,12 +1,12 @@
 module Util where
 
-import BV
+import qualified BV
 
 import Data.Monoid (mempty)
 import Text.Trifecta (parseString, Result(..))
 
-readProgram :: String -> Program
+readProgram :: String -> BV.Program
 readProgram str = 
-  case parseString parseProgram mempty str of
+  case parseString BV.parseProgram mempty str of
     Success x -> x
     Failure doc -> error $ show doc
