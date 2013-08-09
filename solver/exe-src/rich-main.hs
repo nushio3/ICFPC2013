9dd2ad36d39695e55bb9334b77054335f03b0b99
fumieval <fumiexcel@gmail.com>
2013-08-10 08:00:54 +0900

    unlock submitting feature

--- solver/exe-src/rich-main.hs
+++ solver/exe-src/rich-main.hs
@@ -10,6 +10,11 @@ import Text.Printf
 import System.Environment
 import SRichBV (equiv)
 import API
+import Data.Vector.Lens
+import qualified Data.ByteString.Lazy as BL
+import Data.Function
+import Control.Lens.Aeson
+import qualified Data.Aeson as JSON
 
 solveAndAnswer :: Given Token => T.Text -> Int -> [T.Text] -> IO (String, GuessResponse)
 solveAndAnswer tid size ops = do
@@ -29,12 +34,28 @@ main = give (Token "0017eB6c6r7IJcmlTb3v4kJdHXt1re22QaYgz0KjvpsH1H") $ getArgs >
         putStrLn $ "Expected answer: " ++ T.unpack prog
         putStrLn $ "Your answer: " ++ answer
         putStrLn $ "Result: " ++ show res
-    ("submit" : _) -> do
-        putStrLn "This feature is locked"
     ("hoge" : _) -> do
       progs <- solve 9 ["if0","not","shl1","shr16"] equiv
       -- mapM_ (\p -> putStrLn $ printProgram p ++ " -> " ++ printProgram ((canonic.simplify.moveOp2P.moveIfP.simplify.canonic $ p))) progs
       return ()
+    ("submit" : path : t : _) -> do
+        Just vs <- preview (_Just . _Array . from vector) <$> (JSON.decode <$> BL.readFile path :: IO (Maybe JSON.Value))
+        solveAll $ filter ((<Just (read t)) . preview (ix "size" . _Integer)) vs
 
+solveAll :: Given Token => [JSON.Value] -> IO ()
+solveAll (p:ps) = do
+    putStrLn $ "Problem: " ++ T.unpack (p ^?! ix "id" . _String)
+    (answer, res) <- solveAndAnswer (p ^?! ix "id" . _String) (p ^?! ix "size" . _Integer . from enum) (p ^.. ix "operators" . _Array . from vector . traverse . _String)
+    case guessStatus res of
+        GuessWin -> solveAll ps
+        GuessMismatch -> do
+            putStrLn "Mismatch!"
+            putStrLn $ "Program: " ++ answer
+            putStrLn $ "Input: " ++ show (guessValues res ^?! _Just . ix 0)
+            putStrLn $ "Expected: " ++ show (guessValues res ^?! _Just . ix 1)
+            putStrLn $ "Actual: " ++ show (guessValues res ^?! _Just . ix 2)
+        GuessError -> do
+            putStrLn $ T.unpack (maybe "" id $ guessMessage res)
+        
 -- programs :: IO [Program]
 -- programs = map read . lines <$> getContents
