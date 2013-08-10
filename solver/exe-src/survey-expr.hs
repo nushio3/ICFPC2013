f3e32c59b253e979f56050f558ef394812d2f478
Takayuki Muranushi <muranushi@gmail.com>
2013-08-10 18:20:10 +0900

    different ord in expr

--- solver/exe-src/survey-expr.hs
+++ solver/exe-src/survey-expr.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE MultiWayIf #-}
 module Main where
 
-import Control.Lens
+import Control.Lens  (_3, (^.))
 import Control.Monad
 import Data.List
 import Data.Maybe
@@ -16,11 +16,18 @@ import Text.Printf
 import System.IO.Unsafe
 
 newtype O = O { unO :: Expression}
+newtype O2 = O2 { unO2 :: Expression}
 
 instance Eq O where
   (O a) == (O b) = unsafePerformIO $ equivE a b
 instance Ord O where
-  O a `compare` O b = fromJust $ unsafePerformIO $ ordE a b
+  O a `compare` O b = fromJust $ unsafePerformIO $ ordE2 a b
+
+instance Eq O2 where
+  (O2 a) == (O2 b) = unsafePerformIO $ equivE a b
+instance Ord O2 where
+  O2 a `compare` O2 b = fromJust $ unsafePerformIO $ ordE2 a b
+
 
 equivE :: Expression -> Expression -> IO Bool
 equivE e1 e2 = do
@@ -50,32 +57,63 @@ ordE p1 p2  = do
          (n' .< fromIntegral n) ==> sEvalE p1 [n']  .== sEvalE p2 [n']
       if 
         | "Q.E.D." `isInfixOf` show resp       -> return $Just True
-        | "Falsifiable." `isInfixOf` show resp -> return $Just True
+        | "Falsifiable." `isInfixOf` show resp -> return $Just False
         | otherwise                            -> do
            print resp
            return Nothing
-         
+
+
+ordE2 :: Expression -> Expression ->  IO (Maybe Ordering)
+ordE2 p1 p2  = do
+  p1 `equivE` p2 >>= \case 
+    True -> return $ Just EQ
+    False -> do
+      
+      ret1 <- sat $ \x -> do       
+        forAll ["y"] $ \y ->
+          ((x .< y) ==> (sEvalE p1 [y]  .== sEvalE p2 [y])) &&&
+          sEvalE p1 [x]  .< sEvalE p2 [x] 
+      ret2 <- sat $ \x -> do       
+        forAll ["y"] $ \y ->
+          ((x .< y) ==> (sEvalE p1 [y]  .== sEvalE p2 [y])) &&&
+          sEvalE p1 [x]  .> sEvalE p2 [x] 
+      if | "Satisfiable"   `isInfixOf` show ret1 -> return $Just LT
+         | "Satisfiable" `isInfixOf` show ret2   -> return $Just GT
+         | otherwise -> do
+             print ret1
+             print ret2
+             return Nothing
+
 
 
 
 
 allOps = [If0 , TFold , Fold0 , Not , Shl 1 , Shr 1 , Shr 4 , Shr 16 , And , Or , Xor , Plus]
 
-someOps = [ If0 , Shl 1 , Shr 1 , And , Or ,Xor]
+someOps = [ If0 , Shr 1 , And , Or ,Xor]
 
 main :: IO ()
 main = do
-  let exprs = map (^. _3) $ genExpression 6 someOps someOps 1
+  let exprs = map (^. _3) $ genExpression 5 someOps someOps 1
       simpExprs = Set.toList $ Set.fromList $ 
         map simplifyE exprs  
---      nubExprs = nubBy (\x y -> unsafePerformIO $ equivE x y) simpExprs
-      nubExprs2 = 
+      nubExprs0 = nubBy (\x y -> unsafePerformIO $ equivE x y) simpExprs
+      nubExprs = 
         map unO $
         Set.toList $ Set.fromList $ 
         map O $
         simpExprs
+      nubExprs2 = 
+        map unO2 $
+        Set.toList $ Set.fromList $ 
+        map O2 $
+        simpExprs
+  print =<< ordE (exprs!!0)  (exprs!!10) 
+  print =<< ordE2 (exprs!!0)  (exprs!!10) 
   printf "#(expr) = %d\n" $ length exprs  
   printf "#(expr) = %d\n" $ length simpExprs
+  printf "#(expr) = %d\n" $ length nubExprs0
+  printf "#(expr) = %d\n" $ length nubExprs
   printf "#(expr) = %d\n" $ length nubExprs2
   return ()
   
--- solver/src/Gulwani3.hs
+++ solver/src/Gulwani3.hs
@@ -1,4 +1,4 @@
-module Gulwani2 where
+module Gulwani3 where
 import Data.Bits
 import Control.Monad
 import Text.Printf
@@ -9,54 +9,13 @@ import BV (Op1, Op2)
 import SBV (SBitVector)
 
 
-data GInst a
-  = GIf a a a
-  | G1 Op1 a 
-  | G2 Op2 a a
-
-behave1 :: SBitVector -> SBitVector -> Symbolic SBool
-behave1 x y = do
-  inst <- (exists "inst0" :: Symbolic SWord8)
-  return $ select 
-    [ y .== complement x
-    , y .== x `shiftL`1 
-    , y .== x `shiftR`1 
-    , y .== x `shiftR`4 
-    , y .== x `shiftR`16
-    , y .== 1
-    , y .== 0
-    ] 
-    (false) inst
-
-
-behave2 :: SBitVector -> SBitVector -> Symbolic SBool
-behave2 x0 y0 = do
-  inst0 <- (exists "inst0" :: Symbolic SWord8)
-  tmp0 <- (exists "tmp0" :: Symbolic SWord64)
-  inst1 <- (exists "inst1" :: Symbolic SWord8)
-  
-  let deInst inst x y = select 
-        [ y .== complement x
-        , y .== x `shiftL`1 
-        , y .== x `shiftR`1 
-        , y .== x `shiftR`4 
-        , y .== x `shiftR`16
-        , y .== 1
-        , y .== 0
-        ] 
-        (false) inst
-  
-  return $ deInst inst0 x0 tmp0
-        &&& deInst inst1 tmp0 y0 
-        
-        
 
 type Adr = SWord8
 type Ins = SWord8
 type Val = SWord64
 
-deInstB :: Val ->  Ins -> Val -> Val -> Val -> SBool
-deInstB v  inst a b t = select
+deInstB :: Int -> Val -> Val -> Val -> Val -> SBool
+deInstB inst v a b t = (!!inst)
  [ t .== 0             -- 0    
  , t .== 1             -- 1
  , t .== v             -- 2 
@@ -69,32 +28,12 @@ deInstB v  inst a b t = select
  , t .== a .|. b       -- 9    
  , t .== a `xor` b     --10    
  , t .== a + b         --11    
- ] false inst
-
-behaveB1 :: Val -> Val -> Symbolic SBool
-behaveB1 x0 y0 = do
-  let 
-    load myAddr tgtAddr r 
-      = ite (tgtAddr .== 0) (r .== x0) false
-  
-  inst0 <- (exists "inst0" :: Symbolic Ins)
-  a0 <- (exists "a0" :: Symbolic Adr)
-  b0 <- (exists "b0" :: Symbolic Adr)
-  loadResA0 <- (exists "la0" :: Symbolic Val)
-  loadResB0 <- (exists "lb0" :: Symbolic Val)
-  
-  return $
-    load 0 a0 loadResA0 &&&
-    load 0 b0 loadResB0 &&&
-    deInstB x0 inst0 loadResA0 loadResB0 y0
-
+ ] 
 
 behaveBN :: Int -> Val -> Val -> Symbolic SBool
-behaveBN n x0 y0 = do
+behaveBN size x0 y0 = do
   let allAddrs = [0..n-1] 
       
-  insts <- forM allAddrs 
-    (\i -> exists (printf "inst%d" i) :: Symbolic Ins)
   addrAs <- forM allAddrs 
     (\i -> exists (printf "a%d" i) :: Symbolic Adr)
   addrBs <- forM allAddrs 
