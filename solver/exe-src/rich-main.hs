ffafda011f049928c2e235ab062a353846643b54
fumieval <fumiexcel@gmail.com>
2013-08-10 18:49:12 +0900

    some work

--- solver/exe-src/rich-main.hs
+++ solver/exe-src/rich-main.hs
@@ -12,10 +12,74 @@ import Data.List
 import Data.Function
 import SRichBV (equiv)
 import API
+import Data.Word
+import qualified Data.Map as M
+import System.IO.Unsafe
+import Data.Monoid
+import Database
+import System.Random
+
+niceSolve :: Int -> [String] -> (Program -> Program -> IO Bool) -> IO (Maybe (Context [Word64] [Word64] [String]))
+niceSolve size ops equiv = do
+  let ps = niceGenProgram (fromIntegral size) $ map toOp ops
+      qs = map optimize ps
+      ms :: M.Map Program Program
+      ms = M.fromList $ zip qs ps
+      ss = M.keys ms
+  putStrLn $ "Size: " ++ show size ++ ", " ++ show ops
+  putStrLn $ "Generating " ++ show (length ps) ++ " candidates"
+  putStrLn $ "Simplifies to " ++ show (length ss)
+
+  --mapM_ (print . printProgram) ss
+
+  let go i = do
+        let n = 256
+        vs <- replicateM n randomIO
+        let xs = take n $ [0,1,2,3,4,5,15,16,17,65535,65536,65537] ++ vs
+        let res0  = [ (map (RichBV.eval p) xs, Endo (p:)) | p <- ss]
+            mm0 :: M.Map [Word64] (Endo [Program])
+            mm0   = M.fromListWith mappend res0
+            freq0 = maximum $ map (length . retract) $ M.elems mm0            
+
+        if freq0 <= mismatchTolerance
+          then do
+            return $ Just $ Context (\vss -> [ printProgram $ ms M.! cand | cand <- retract $ mm0 M.! vss]) xs
+          else do
+            if i > retryTimes
+              then do
+                let tt = retract $ snd $ head $ reverse $ sortBy (compare `on` (length . retract . snd)) $ M.toList mm0
+                putStrLn "Cannot divide groups"
+                putStrLn $ "Maximum group size: " ++ show (length tt)
+
+                forM_ (zip [1::Int ..] tt) $ \(ii, x) -> forM_ (zip [1..] tt) $ \(jj, y) -> do
+                  when (ii < jj) $ do
+                    b <- x `equiv` y
+                    when b $ do
+                      putStrLn $ printProgram x
+                      putStrLn $ printProgram y
+                      putStrLn "==="
+
+                putStrLn "Trying to resolve..."
+                let res1  = [ (map (RichBV.eval p) xs, Endo (merger p)) | p <- ss] 
+                    mm1 :: M.Map [Word64] (Endo [Program])
+                    mm1   = M.fromListWith mappend res1
+                    
+                    merger :: Program -> [Program] -> [Program]
+                    merger x xs 
+                      | any (unsafePerformIO . equiv x) xs = xs
+                      | otherwise        = x:xs
+
+                    freq1 = maximum $ map (length . retract) $ M.elems mm1
+                if freq1 < mismatchTolerance
+                  then return $ Just $ Context (\vss -> [ printProgram $ ms M.! cand | cand <- retract $ mm1 M.! vss]) xs
+                  else return Nothing
+              else go (i + 1)
+
+  go 0 :: IO (Maybe (Context [Word64] [Word64] [String]))
 
 solveAndAnswer :: Given Token => T.Text -> Int -> [T.Text] -> IO (Maybe (String, GuessResponse))
 solveAndAnswer tid size ops = do
-  rr <- solve size (map T.unpack ops) equiv
+  rr <- niceSolve size (map T.unpack ops) equiv
   case rr of
     Just (Context restore query) -> do
       EvalResponse _ (Just eout) _ <- API.eval
--- solver/exe-src/survey-expr.hs
+++ solver/exe-src/survey-expr.hs
@@ -28,41 +28,6 @@ instance Eq O2 where
 instance Ord O2 where
   O2 a `compare` O2 b = fromJust $ unsafePerformIO $ ordE2 a b
 
-
-equivE :: Expression -> Expression -> IO Bool
-equivE e1 e2 = do
-  res <- prove $ \x -> sEvalE e1 [x] .== sEvalE e2 [x]
-  return $ "Q.E.D." `isPrefixOf` show res
-
-ordE :: Expression -> Expression ->  IO (Maybe Ordering)
-ordE p1 p2  = do
-  p1 `equivE` p2 >>= \case 
-    True -> return $ Just EQ
-    False -> search minBound maxBound
-
-  
-  where
-    search a b
-      | a+1 == b = return $ Just $ evalE  p1 [b] `compare` evalE  p2 [b]
-      | otherwise = do
-          let c = div (a+1) 2 + div b 2 
-          go c >>= \case
-            Just True  -> search c b
-            Just False -> search a c
-            Nothing    -> return Nothing
-
-    go :: BitVector -> IO (Maybe Bool)
-    go n = do
-      resp <- prove $ \n' -> 
-         (n' .< fromIntegral n) ==> sEvalE p1 [n']  .== sEvalE p2 [n']
-      if 
-        | "Q.E.D." `isInfixOf` show resp       -> return $Just True
-        | "Falsifiable." `isInfixOf` show resp -> return $Just False
-        | otherwise                            -> do
-           print resp
-           return Nothing
-
-
 ordE2 :: Expression -> Expression ->  IO (Maybe Ordering)
 ordE2 p1 p2  = do
   p1 `equivE` p2 >>= \case 
