d1650643c6ba7180630085661e9bf6ca46471e39
Takayuki Muranushi <muranushi@gmail.com>
2013-08-11 00:34:00 +0900

    well-formedness theorem separated.

--- solver/src/Gulwani4.hs
+++ solver/src/Gulwani4.hs
@@ -12,13 +12,26 @@ import Text.Printf
 type Addr = SWord16
 type Val = SWord64
 
-type LProgram = (Addr, [Opaddr Addr])
-
 data Op = Const Int | Var 
         | Not | Shl Int | Shr Int
         | And | Or | Xor | Plus
         | If0 
                            deriving (Eq, Ord, Show, Read)
+
+
+data Opaddr a = Opaddr 
+  {_inst :: Op, _ovar :: a, _ivars :: [a]}
+    
+$(makeLenses ''Opaddr)  
+
+
+data LVProgram = 
+  LVProgram
+    { _returnAddr :: Addr, 
+      _library ::[Opaddr Addr]}
+  
+$(makeLenses ''LVProgram)  
+
 
 symbolicOp :: Int -> Op -> Int -> Symbolic (Opaddr Addr)
 symbolicOp i opr arity = do
@@ -27,35 +40,59 @@ symbolicOp i opr arity = do
     (exists (printf "ia-%d-%d" i j) :: Symbolic Addr) 
   return $ Opaddr opr oaddr iaddrs
   
-data Opaddr a = Opaddr 
-  {_inst :: Op, _ovar :: a, _ivars :: [a]}
-    
-$(makeLenses ''Opaddr)  
 
 
-testProg :: Symbolic LProgram
+testProg :: Symbolic (SBool, LVProgram)
 testProg = do
   ret <- symbolic "returnAddr"
   a <- symbolicOp 0 Var 0
   b <- symbolicOp 1 Plus 2
   c <- symbolicOp 2 Plus 2
-  return (ret, [a,b,c])
+  let 
+    addrLib :: [Opaddr Addr]
+    addrLib = [a,b,c]
+    n = length addrLib
+  let 
+    allAddrs :: [Addr]
+    allAddrs = map (^.ovar) addrLib
+      ++ concat (map (^.ivars) addrLib)
+    
+    outAddrs :: [Addr]
+    outAddrs = map (^.ovar) addrLib
 
+    phiAddrBound :: Addr -> SBool
+    phiAddrBound a = 0 .<= a &&& a .< (fromIntegral n)
+    
+    phiAcyc :: Opaddr Addr -> SBool
+    phiAcyc oa = bAll (.<(oa^.ovar)) (oa^.ivars)
+    
+    thmWfp = 
+      -- bounded
+      bAll phiAddrBound allAddrs &&&
+      -- consistency      
+      bAnd [(outAddrs!!i)./= (outAddrs!!j) 
+           | i <- [0..n-1], j <- [i+1..n-1]] &&& 
+      -- acyclicity
+      bAll phiAcyc addrLib
+  
+  return (thmWfp, LVProgram ret addrLib)
 
 test a b = do
-  prog <- testProg
-  phiFunc prog a b
+  (thmWfp, prog) <- testProg
+  thmBeh <- phiFunc prog a b
+  return $ thmWfp &&& thmBeh
 
 
-phiFunc :: LProgram -> Val -> Val -> Symbolic SBool
-phiFunc (retAddr, library) alpha beta = do
-  let n = length library
+phiFunc :: LVProgram -> Val -> Val -> Symbolic SBool
+phiFunc lvProg alpha beta = do
+  let n = length $ addrLib
+      addrLib =  lvProg ^. library
       allAddrs = [0..n-1]  
       
-  outVals <- (\k -> zipWithM k allAddrs library) $
+  outVals <- (\k -> zipWithM k allAddrs addrLib) $
     \i opaddr -> (exists (printf "out-%d" i) :: Symbolic Val) 
 
-  varLib <- (\k -> zipWithM k allAddrs library) $
+  varLib <- (\k -> zipWithM k allAddrs addrLib) $
     \i opaddr -> do
       ivals <- 
         sequence [ exists (printf "in-%d-%d" i j) :: Symbolic Val 
@@ -65,8 +102,6 @@ phiFunc (retAddr, library) alpha beta = do
   let phiConn :: (Addr,Val) -> (Addr, Val) -> SBool
       phiConn (a1,v1) (a2,v2) = (a1 .== a2) ==> (v1 .== v2)
       
-      phiAddrBound :: Addr -> SBool
-      phiAddrBound a = 0 .<= a &&& a .< (fromIntegral n)
 
   let phiLib :: Opaddr Val -> SBool
       phiLib opaddr = 
@@ -74,32 +109,30 @@ phiFunc (retAddr, library) alpha beta = do
         case (opaddr^.inst, opaddr^.ivars) of
           (Const n, [])  -> fromIntegral n
           (Var, [])      -> alpha
-          (Or , [a,b])   -> a .&. b
-          (Xor , [a,b])  -> a .|. b
+          (Not, [a])     -> complement a
+          (Shl n, [a])   -> shiftL a n
+          (Shr n, [a])   -> shiftR a n
+          (And , [a,b])  -> a .&. b
+          (Or , [a,b])   -> a .|. b
+          (Xor , [a,b])  -> a `xor` b
           (Plus , [a,b]) -> a + b
           (If0, [a,b,c]) -> ite (a .== 0) b c
           _              -> undefined
           
   let allAVs :: [(Addr,Val)]
-      allAVs = (retAddr, beta):avLibs
+      allAVs = (lvProg ^. returnAddr, beta):avLibs
       
       nAllAVs = length allAVs
       
       avLibs = concat $
-        (\k -> zipWith k library varLib) $ 
+        (\k -> zipWith k addrLib varLib) $ 
           \opA opV -> (opA^.ovar, opV^.ovar) : 
                       zip (opA^.ivars) (opV^.ivars)
       outAddrs :: [Addr]
-      outAddrs = map (^.ovar) library
+      outAddrs = map (^.ovar) addrLib
   
-      phiAcyc :: Opaddr Addr -> SBool
-      phiAcyc oa = bAll (.<(oa^.ovar)) (oa^.ivars)
   
   return $ 
-    bAnd (map (phiAddrBound . fst) allAVs) &&&
-    bAnd [(outAddrs!!i)./= (outAddrs!!j) 
-         | i <- [0..n-1], j <- [i+1..n-1]] &&& -- consistency
-    bAll phiAcyc library &&&
     bAnd (map phiLib varLib) &&&
     bAnd [ phiConn (allAVs!!i) (allAVs!!j) 
          | i <- [0..nAllAVs-1], j <- [i+1 .. nAllAVs-1]]
