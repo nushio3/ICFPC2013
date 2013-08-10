2d521abc311daa618097c6100d59fac871364740
fumieval <fumiexcel@gmail.com>
2013-08-10 18:28:51 +0900

    memoized generator

--- solver/exe-src/survey-expr.hs
+++ solver/exe-src/survey-expr.hs
@@ -10,44 +10,9 @@ import qualified Data.Set as Set
 
 import BV(BitVector)
 import RichBV
-import SRichBV (sEvalE)
+import SRichBV
 import Text.Printf
 import System.IO.Unsafe
-
-equivE :: Expression -> Expression -> IO Bool
-equivE e1 e2 = do
-  res <- prove $ \x -> sEvalE e1 [x] .== sEvalE e2 [x]
-  return $ "Q.E.D." `isPrefixOf` show res
-
-ordE :: Expression -> Expression ->  IO (Maybe Ordering)
-ordE p1 p2  = do
-  p1 `equivE` p2 >>= \case 
-    True -> return $ Just EQ
-    False -> search minBound maxBound
-
-  
-  where
-    search a b
-      | a+1 == b = return $ Just $ evalE  p1 [b] `compare` evalE  p2 [b]
-      | otherwise = do
-          let c = div (a+1) 2 + div b 2 
-          go c >>= \case
-            Just True  -> search c b
-            Just False -> search a c
-            Nothing    -> return Nothing
-
-    go :: BitVector -> IO (Maybe Bool)
-    go n = do
-      resp <- prove $ \n' -> 
-         (n' .< fromIntegral n) ==> sEvalE p1 [n']  .== sEvalE p2 [n']
-      if 
-        | "Q.E.D." `isInfixOf` show resp       -> return $Just True
-        | "Falsifiable." `isInfixOf` show resp -> return $Just True
-        | otherwise                            -> do
-           print resp
-           return Nothing
-         
-
 
 
 
