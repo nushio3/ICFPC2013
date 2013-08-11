3550a6ce4d724821f0c2ba89c07b0722f7ca7e36
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2013-08-11 15:18:31 +0900

    no message

--- solver/exe-src/smt-synth.hs
+++ solver/exe-src/smt-synth.hs
@@ -10,6 +10,7 @@ import System.Environment
 import Data.Maybe
 import Control.Applicative
 import Control.Monad.Trans
+import Data.List
 
 import API
 
@@ -17,9 +18,16 @@ import SMTSynth
 
 main :: IO ()
 main = give (Token "0017eB6c6r7IJcmlTb3v4kJdHXt1re22QaYgz0KjvpsH1H") $ getArgs >>= \case
-  ("training" : level : _) -> do
+  ("training": level: _) -> do
     TrainingProblem prog ident size ops <- train $ TrainRequest (read level) []
+    putStrLn $ "Problem description: " ++ T.unpack ident ++ " " ++ show size ++ " " ++ unwords (map T.unpack ops)
     putStrLn $ "Expected answer: " ++ T.unpack prog
+    putStrLn ""
     synth size ops ident
+  ["specify", tid] -> do
+    problems <- myproblems
+    case find (\p -> problemId p == T.pack tid) problems of
+      Just p  -> synth (problemSize p) (problemOperators p) (problemId p)
+      Nothing -> fail "No such problem"
   _ -> do
-    undefined
+    fail "invalid input ('_`)"
