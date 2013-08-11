e96891adc8a1fbe304a520f1ac0e51fbcebb24d9
Yu Sugawara <gusmachine+git@gmail.com>
2013-08-12 08:31:00 +0900

    don't kill yourself.

--- solver/exe-src/smt-synth.hs
+++ solver/exe-src/smt-synth.hs
@@ -33,7 +33,7 @@ main = give (Token "0017eB6c6r7IJcmlTb3v4kJdHXt1re22QaYgz0KjvpsH1H") $ getArgs >
       Just p  -> synth (read cpu) (problemSize p) (problemOperators p) (problemId p)
       Nothing -> fail "No such problem"
   ("auto": cpu:flds) -> do
-    system "cd ..; ./mapssh S '(4,9)' 'killall smt-synth' | sh"
+    -- system "cd ..; ./mapssh S '(4,9)' 'killall smt-synth' | sh"
     putStrLn "You start automatic solving mode, really?"
     "y" <- getLine
     allProblems <- myproblems
