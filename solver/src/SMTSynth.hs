79dfdc1a9511e431671f1980c705644304a29899
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2013-08-12 01:19:15 +0900

    no message

--- solver/src/SMTSynth.hs
+++ solver/src/SMTSynth.hs
@@ -408,14 +408,14 @@ synth cpuNum ss ops' ident = if "fold" `elem` ops' then putStrLn "I can not use 
   let go es = do
         -- putStrLn "behave..."
         putStrLn $ "inputs: " ++ show es
-        progn <- para cpuNum $ \i -> findProgram i myFlags oprs (size + i `mod` 4) $ take 5 es
+        progn <- para cpuNum $ \i -> findProgram i myFlags oprs (size + i `mod` 3) $ take 5 es
         system "pkill z3"
         putStrLn $ "found: " ++ (BV.printProgram $ toProgram myFlags oprs progn)
         o <- oracleDistinct ident $ toProgram myFlags oprs progn
         case o of
           Nothing -> do
             putStrLn "Accepted: yatapo-(^_^)!"
-            system "wget http://botis.org:9999/play/VEC1%20FX%20081.wav -O /dev/null"
+            system "wget http://botis.org:9999/play/meow.wav -O /dev/null"
             return ()
           Just oo -> do
             putStrLn $ "distinct: " ++ show oo
